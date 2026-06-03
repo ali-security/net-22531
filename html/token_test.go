6be38bb420749285618dddffc57b51770a192596
Seal <lev.p@sealsecurity.io>
2026-06-03 11:36:10 +0000

    CVE-2025-22872

--- html/token_test.go
+++ html/token_test.go
@@ -601,6 +601,16 @@ var tokenTests = []tokenTest{
 		`<p  =asd>`,
 		`<p =asd="">`,
 	},
+	{
+		"slash at end of unquoted attribute value",
+		`<p a="\">`,
+		`<p a="\">`,
+	},
+	{
+		"self-closing tag with attribute",
+		`<p a=/>`,
+		`<p a="/">`,
+	},
 }
 
 func TestTokenizer(t *testing.T) {
@@ -797,6 +807,14 @@ func TestReaderEdgeCases(t *testing.T) {
 			t.Errorf("i=%d: got %v, want %v", i, got, want)
 			continue
 		}
+	}
+}
+
+func TestSelfClosingTagValueConfusion(t *testing.T) {
+	z := NewTokenizer(strings.NewReader(`<p a=/>`))
+	tok := z.Next()
+	if tok != StartTagToken {
+		t.Fatalf("unexpected token type: got %s, want %s", tok, StartTagToken)
 	}
 }
 
